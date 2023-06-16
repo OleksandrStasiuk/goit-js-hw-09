f33989ceabfa317b527f4e293cb093b17bb7221c
OleksandrStasiuk <122239424+OleksandrStasiuk@users.noreply.github.com>
2023-06-16 14:48:48 +0300

    finish

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const bodyRef = document.querySelector('body');
+let timerId = null;
 
+startBtn.addEventListener('click', changeColorTheme);
+stopBtn.addEventListener('click', stopColorTheme);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function changeColorTheme() {
+  timerId = setInterval(() => {
+    bodyRef.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+
+  stopBtn.removeAttribute('disabled');
+  startBtn.setAttribute('disabled', 'disabled');
+}
+
+function stopColorTheme() {
+  clearInterval(timerId);
+
+  stopBtn.setAttribute('disabled', 'disabled');
+  startBtn.removeAttribute('disabled');
+}
